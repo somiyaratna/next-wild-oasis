d7477e54ea9687abb9275189888ccf05aba4604a
somiyaratna <beherasomiyaratna@gmail.com>
2025-04-02 13:50:51 +0530

    Reservations and Account Route

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,5 +1,9 @@
 import ReservationCard from "@/app/_components/ReservationCard";
 
+export const metadata = {
+  title: "Your reservations",
+};
+
 export default function Page() {
   // CHANGE
   const bookings = [];
--- app/layout.js
+++ app/layout.js
@@ -27,8 +27,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
         <Header />
-        <main className="flex-1 px-8 py-12">
-          <div className="max-w-7xl mx-auto px-8">{children}</div>
+        <main className="flex-1 px-8 py-12 grid">
+          <div className="max-w-7xl mx-auto px-8 w-full">{children}</div>
         </main>
       </body>
     </html>
