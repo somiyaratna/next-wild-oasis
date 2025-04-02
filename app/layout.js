d18da6aa5e08bfada37a210830ca552aef13fe8b
somiyaratna <beherasomiyaratna@gmail.com>
2025-04-02 12:23:05 +0530

    Added josefin_sans font

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,14 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-josefin-sans",
+});
+
 export const metadata = {
   title: {
     template: "%s | Next Wild Oasis",
@@ -14,7 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
