bd091f84c2c2c57955b088acd96189250e9b1a59
somiyaratna <beherasomiyaratna@gmail.com>
2025-04-07 10:14:51 +0530

    display user image on homepage

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
+import Image from "next/image";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+  console.log(session);
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +25,29 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center gap-4"
+            >
+              <Image
+                width={20}
+                height={20}
+                className="h-8 w-8 rounded-full"
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy="no-referrer"
+              />
+              <span>{session.user.name}</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
