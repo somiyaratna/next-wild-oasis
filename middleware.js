d0538e60a47eea9e2ab486e048c49754c75f108e
somiyaratna <beherasomiyaratna@gmail.com>
2025-04-07 11:34:45 +0530

    added middleware to account page

---
+++ middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+// export function middleware(req) {
+//   console.log(req);
+
+//   return NextResponse.redirect(new URL("/about", req.url));
+// }
+
+import { auth } from "./app/_lib/auth";
+export const middleware = auth;
+
+export const config = {
+  matcher: ["/account"],
+};
