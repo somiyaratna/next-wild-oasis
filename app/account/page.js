30b9e68b8162e5fdf8c91e3e731d12d08941f9d2
somiyaratna <beherasomiyaratna@gmail.com>
2025-04-08 12:17:43 +0530

    supabase partial connection

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,11 @@
-export default function Page() {
+import { auth } from "../_lib/auth";
+
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ")[0];
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-4">
-      Welcome, User
+      Welcome, {firstName}
     </h2>
   );
 }
